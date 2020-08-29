b6f87e744cbdeead49279d231ec8ef2d5a15bfb6
Elaine <elainemattje@gmail.com>
2020-08-29 10:20:18 -0300

    created recuperar senha

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Cadastro from './Pages/Cadastro';
 import InseriNota from './Pages/Cadastro';
+import RecuperarSenha from './Pages/RecuperarSenha';
 
-function Router(){
-    return(
+function Router() {
+    return (
         <BrowserRouter>
-            <Route path="/" exact component={Login}/>
-            <Route path="/cadastro" exact component={Cadastro}/>
+            <Route path="/" exact component={Login} />
+            <Route path="/cadastro" exact component={Cadastro} />
+            <Route path="/recuperarSenha" exact component={RecuperarSenha} />
         </BrowserRouter>
     )
 }
